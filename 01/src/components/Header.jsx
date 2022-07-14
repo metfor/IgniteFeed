0b515b9323cff8cb2e1be35a41154513ea58237d
Matheus Pereira Frade <matheus.frade@pixter.com.br>
2022-07-14 15:21:11 -0300

    feat(ignitefeed): header

--- 01/src/components/Header.jsx
+++ 01/src/components/Header.jsx
@@ -1,8 +1,9 @@
 import styles from "./Header.module.css";
+import igniteLogo from "../public/Header/logo.svg";
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>ignite</strong>
+      <img src={igniteLogo} />
     </header>
   );
 }
