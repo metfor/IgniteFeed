1d1f372ced53801409c2a681e79a85ea98fec480
Matheus Pereira Frade <matheus.frade@pixter.com.br>
2022-07-14 14:41:50 -0300

    feat(ignitefeed): css modules

--- 01/src/App.jsx
+++ 01/src/App.jsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
-
+import "./styles.css";
 export function App() {
   return (
     <>
+      <Header />
       <Post
         author="frade"
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis facilis ad laborum fugiat magnam eligendi eaque quaerat nihil, dignissimos totam veritatis! Quos quasi tenetur, at odit saepe consequuntur culpa amet."
