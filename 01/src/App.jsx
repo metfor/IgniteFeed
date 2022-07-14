e2a24a39536fd0cb7c6ef1ab0d8d07acf0a58817
Matheus Pereira Frade <matheus.frade@pixter.com.br>
2022-07-14 15:59:31 -0300

    feat(ignitefeed): sidebar

--- 01/src/App.jsx
+++ 01/src/App.jsx
@@ -1,14 +1,22 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import styles from "./App.module.css";
 import "./global.css";
+import { SideBar } from "./components/SideBar";
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="frade"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis facilis ad laborum fugiat magnam eligendi eaque quaerat nihil, dignissimos totam veritatis! Quos quasi tenetur, at odit saepe consequuntur culpa amet."
-      />
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          {" "}
+          <Post
+            author="frade"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis facilis ad laborum fugiat magnam eligendi eaque quaerat nihil, dignissimos totam veritatis! Quos quasi tenetur, at odit saepe consequuntur culpa amet."
+          />
+        </main>
+      </div>
     </>
   );
 }
