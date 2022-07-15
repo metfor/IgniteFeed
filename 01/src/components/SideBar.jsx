d29ffe94bf5fd7c88c8ce510296a8e6266613f85
Matheus Pereira Frade <matheus.frade@pixter.com.br>
2022-07-15 11:20:46 -0300

    feat(ignitefeed): sidebar

--- 01/src/components/SideBar.jsx
+++ 01/src/components/SideBar.jsx
@@ -1,4 +1,5 @@
 import styles from "./SideBar.module.css";
+import { PencilLine } from "phosphor-react";
 
 export function SideBar() {
   return (
@@ -9,11 +10,19 @@ export function SideBar() {
           src="https://images.unsplash.com/photo-1571862777746-c59ea9425ea2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
         />
         <div className={styles.profile}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/metfor.png"
+            alt="perfil-foto"
+          />
           <strong>Matheus frade</strong>
           <span>Web Developer</span>
         </div>
         <footer>
-          <a href="#">Editar seu perfil</a>
+          <a href="#">
+            <PencilLine size={20} />
+            Editar seu perfil
+          </a>
         </footer>
       </aside>
     </>
