f8ac693bd74427e6bc1aea3f73847b9ff3347bc6
Matheus Pereira Frade <matheus.frade@pixter.com.br>
2022-07-18 16:27:16 -0300

    feat(ignitefeed): component Comment

---
+++ 01/src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/metfor.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Matheus Frade</strong>
+              <time title="15/07/2022" dateTime="2022-07-15">
+                adadd
+              </time>
+            </div>
+            <button title="deletar comentario">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>legas sadghdfsfd</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
